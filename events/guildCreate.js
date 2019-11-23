bb595ffdd20a8adaf887105a1df74a236274a878
jagu0001 <jagu0001@student.monash.edu>
2019-11-23 18:40:40 +1100

    Fixed bug on when guild joins a server

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,6 +1,6 @@
 let AllGuildData = require("../guilds/guildData");
 
-module.exports = function(guild){
+module.exports = function(bot, guild){
     console.log("DojoBot has joined a guild!");
     console.log("Guild name: " + guild.name);
     console.log("Owner tag: " + guild.owner.user.tag);
